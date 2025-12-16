d270af69b4759720ea592638d8f83b8d20f0219a
ThatianaMattos <thatianamattos25@gmail.com>
2025-12-16 01:51:11 -0300

    Atualização do Deploy - Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
+      useClass: ProdServiceService,
       imports: [ConfigModule],
     }),
     PostagemModule,
